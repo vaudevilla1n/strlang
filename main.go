8b349ba83edd5fd874258eff2b21042542cae8a1
Jake Faard <jakefaard@gmail.com>
2025-12-27 13:51:17 -0500

    defining grammar

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+/*
+	grammar
+
+	expression	::= term
+	term		::= primary '+' primary
+	primary		::= func | string | group
+	func		::= builtin '::(' expression (',' expression)* ')'
+	string		::= string literal
+	group		::= '(' expression ')'
+	builtin		::= any builtin function
+
+	ast
+
+	expression	::= binary | func | group | literal
+	binary		::= expression operator expression
+	func		::= builtin argument_count expression+
+	group		::= '(' expression ')'
+	literal		::= string literal
+	operator	::= '+'
+*/
+
 import (
 	strlang/lexer"
 	"strlang/quote"
